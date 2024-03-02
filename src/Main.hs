e697a91512b5931aeb87b44b50e4500a736709d1
nigosto <nigosto@gmail.com>
2024-03-02 17:10:37 +0200

    Remove the pretty-lambda option

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,13 @@
 module Main where
 
 import System.IO (hSetBuffering, stdout, BufferMode (BlockBuffering))
-import System.Environment (getArgs)
-import Parser (parseTerm)
+import Parser (parseTerm, Token (Variable))
 import Substitution (substitute)
 import Generator (generateTerm)
 
 main :: IO ()
 main = do
   hSetBuffering stdout $ BlockBuffering $ Just 1
-  args <- getArgs
   term <- putStr "Input term: " >> getLine
   var <- putStr "Input variable to substitute: " >> getLine -- not using getChr intentionally
   if length var /= 1 
@@ -17,6 +15,4 @@ main = do
   else do
     let substituteVariable = head var
     substituteTerm <- putStr "Input substitution term: " >> getLine
-    let generatedTerm = generateTerm $ substitute (parseTerm term) substituteVariable $ parseTerm substituteTerm
-    let termToPrint = if "pretty-lambda" `elem` args then map (\x -> if x == 'λ' then '\\' else x) generatedTerm else generatedTerm
-    putStrLn termToPrint
+    putStrLn $ generateTerm $ substitute (parseTerm term) substituteVariable (parseTerm substituteTerm)
