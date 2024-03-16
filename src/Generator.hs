4149682174c1d42e4cdd3b6ec3d915c8472dec96
nigosto <nigosto@gmail.com>
2024-03-16 20:39:18 +0200

    Reduce the number of generated brackets

--- src/Generator.hs
+++ src/Generator.hs
@@ -4,14 +4,28 @@ import Transformer (NamelessTerm (NamelessVariable, NamelessApplication, Nameles
 
 generateTerm :: Term -> String
 generateTerm (Variable var) = [var]
-generateTerm (Application lhs rhs) = concatMap (\term -> case term of
-  var@(Variable _) -> generateTerm var
-  _ -> '(':generateTerm term ++ [')']) [lhs, rhs]
+generateTerm (Application lhs rhs) =
+  let generatedLhs = case lhs of
+        var@(Variable _) -> generateTerm var  
+        application@(Application _ _) -> generateTerm application
+        abstraction@(Abstraction _ _ ) -> '(':generateTerm abstraction ++ [')']
+      generatedRhs = case rhs of
+        var@(Variable _) -> generateTerm var
+        application@(Application _ _) -> '(':generateTerm application ++ [')']
+        abstraction@(Abstraction _ _) -> '(':generateTerm abstraction ++ [')']
+  in generatedLhs ++ generatedRhs
 generateTerm (Abstraction argument body) = 'λ':argument:'.':generateTerm body
 
 generateNamelessTerm :: NamelessTerm -> String
 generateNamelessTerm (NamelessVariable var) = show var
-generateNamelessTerm (NamelessApplication lhs rhs) = concatMap (\term -> case term of
-  var@(NamelessVariable _) -> generateNamelessTerm var
-  _ -> '(':generateNamelessTerm term ++ [')']) [lhs, rhs]
+generateNamelessTerm (NamelessApplication lhs rhs) =
+  let generatedLhs = case lhs of
+        var@(NamelessVariable _) -> generateNamelessTerm var  
+        application@(NamelessApplication _ _) -> generateNamelessTerm application
+        abstraction@(NamelessAbstraction _ ) -> '(':generateNamelessTerm abstraction ++ [')']
+      generatedRhs = case rhs of
+        var@(NamelessVariable _) -> generateNamelessTerm var
+        application@(NamelessApplication _ _) -> '(':generateNamelessTerm application ++ [')']
+        abstraction@(NamelessAbstraction _) -> '(':generateNamelessTerm abstraction ++ [')']
+  in generatedLhs ++ generatedRhs
 generateNamelessTerm (NamelessAbstraction body) = 'λ':generateNamelessTerm body
